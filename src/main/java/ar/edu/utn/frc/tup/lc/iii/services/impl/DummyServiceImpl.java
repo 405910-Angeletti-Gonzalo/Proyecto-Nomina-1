fa71bfe5b41ffc3c661580f9c907dfa09f69966e
405910-Angeletti-Gonzalo <405910@tecnicatura.frc.utn.edu.ar>
2025-11-18 00:07:32 -0300

    Limpieza de codigo

--- src/main/java/ar/edu/utn/frc/tup/lc/iii/services/impl/DummyServiceImpl.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iii/services/impl/DummyServiceImpl.java
@@ -1,6 +1,5 @@
 package ar.edu.utn.frc.tup.lc.iii.services.impl;
 
-import ar.edu.utn.frc.tup.lc.iii.dtos.DummyDto;
 import ar.edu.utn.frc.tup.lc.iii.entities.DummyEntitie;
 import ar.edu.utn.frc.tup.lc.iii.models.Dummy;
 import ar.edu.utn.frc.tup.lc.iii.repositories.DummyRepository;
@@ -8,15 +7,12 @@ import ar.edu.utn.frc.tup.lc.iii.services.DummyService;
 import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.http.HttpStatus;
-
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DummyServiceImpl implements DummyService {
@@ -37,24 +33,23 @@ private DummyRepository dummyRepository;
     public List<Dummy> getDummyList() {
         List<Dummy> dummyList = new ArrayList<>();
         List<DummyEntitie> dummyEntities = dummyRepository.findAll();
-        for (int i = 0; i < dummyEntities.size(); i++) {
-            dummyList.add(modelMapper.map(dummyEntities.get(i), Dummy.class));
+        for (DummyEntitie dummyEntity : dummyEntities) {
+            dummyList.add(modelMapper.map(dummyEntity, Dummy.class));
         }
         return dummyList;
     }
 
     @Override
     public Dummy createDummy(Dummy dummy) {
-        DummyEntitie dummyEntitie = new DummyEntitie();
+        DummyEntitie dummyEntitie;
         dummyEntitie = modelMapper.map((dummy), DummyEntitie.class);
         dummyRepository.save(dummyEntitie);
-        Dummy dummy1 = modelMapper.map(dummyEntitie, Dummy.class);
-        return dummy1;
+        return modelMapper.map(dummyEntitie, Dummy.class);
     }
 
     @Override
     public Dummy updateDummy(Dummy dummy) {
-        Dummy mDummy = new Dummy();
+        Dummy mDummy;
         DummyEntitie dummyEntitie = dummyRepository.save(modelMapper.map(dummy, DummyEntitie.class));
         dummyRepository.save(dummyEntitie);
         mDummy = modelMapper.map(dummyEntitie, Dummy.class);
